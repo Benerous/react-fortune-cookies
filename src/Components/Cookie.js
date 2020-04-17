c1d04634a279b3b765e461ae78e1c624fb84f89b
Benerous <Benerous@users.noreply.github.com>
2020-04-17 16:01:38 +0300

    Added list of languages, ukrainian phrases

--- src/Components/Cookie.js
+++ src/Components/Cookie.js
@@ -2,17 +2,25 @@ import React, { Component } from 'react'
 
 export default class Cookie extends Component {
     render() {
-        const { message, handleChange, phraseVisibility, handleReset } = this.props;
+        const { message, handleChange, phraseVisibility, handleReset, language } = this.props;
         return (
-            <div className="container">
+            <div className="container pt-5">
                 <div>
                     <div className="mx-auto">
                         <img className={phraseVisibility ? 'fadeOut':'fadeIn cookie'} onClick={handleChange} src="https://www.online-fortune-telling.com/images/saxumpicker/rose/fortune-cookie.png" alt="cookie" />
                     </div>
                     <div className="cookie mx-auto">
                         <div className={phraseVisibility ? 'fadeIn-button pt-5':'fadeOut-button pt-5'}>
-                            <p> {message} </p>
-                            <button className="btn btn-outline-dark" onClick={handleReset}>Try again</button>
+                            <h4> {message} </h4>
+                            <div className={phraseVisibility ? 'fadeIn-button':'invisible'}>
+                                <button className="btn btn-outline-dark mt-5" onClick={handleReset}>
+                                    {
+                                        language === "Eng" ? "Try again" : 
+                                            language === "Ukr" ? "Спробуй ще раз" : 
+                                            "Попробуй еще раз"
+                                    } 
+                                </button>
+                            </div>
                         </div>
                     </div>
                 </div>
