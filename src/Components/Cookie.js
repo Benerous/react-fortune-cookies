f5d7832246c81fc7b195aacf8dfe9df5b56424ba
Benerous <Benerous@users.noreply.github.com>
2020-05-15 13:14:09 +0300

    Items positions fixed

--- src/Components/Cookie.js
+++ src/Components/Cookie.js
@@ -4,27 +4,25 @@ export default class Cookie extends Component {
     render() {
         const { message, handleChange, phraseVisibility, handleReset, language } = this.props;
         return (
-            <div className="container pt-5">
-                <div>
-                    <div className="m-md-5 m-4">
-                        {
-                            language === "Eng" ? <h3 className={phraseVisibility ? 'fadeOut-button':'fadeIn-button'}>Think about the Question and get an Answer to It</h3> : 
-                                <h3 className={phraseVisibility ? 'fadeOut-button':'fadeIn-button'}>Придумай запитання та отримай відповідь на нього</h3>
-                        }
-                    </div>
-                    <div className="mx-auto">
-                        <img className={phraseVisibility ? 'fadeOut':'fadeIn cookie'} onClick={handleChange} src="https://www.online-fortune-telling.com/images/saxumpicker/rose/fortune-cookie.png" alt="cookie" />
-                    </div>
-                    <div className="phrase mx-auto">
-                        <div className={phraseVisibility ? 'fadeIn-button pt-5':'fadeOut-button pt-5'}>
-                            <h4> {message} </h4>
-                            <div className={phraseVisibility ? 'fadeIn-button':'invisible'}>
-                                <button className="btn btn-outline-dark mt-5" onClick={handleReset}>
-                                    {
-                                        language === "Eng" ? "Try again" : "Спробуй ще раз"
-                                    } 
-                                </button>
-                            </div>
+            <div className="row">
+                <div className="text-center p-3 col-12">
+                    {
+                        language === "Eng" ? <h3 className={phraseVisibility ? 'fadeOut-button':'fadeIn-button'}>Think about the Question and get an Answer to It</h3> : 
+                            <h3 className={phraseVisibility ? 'fadeOut-button':'fadeIn-button'}>Придумай запитання та отримай відповідь на нього</h3>
+                    }
+                </div>
+                <div className="text-center p-3 col-12">
+                    <img className={phraseVisibility ? 'fadeOut':'fadeIn cookie'} onClick={handleChange} src="https://www.online-fortune-telling.com/images/saxumpicker/rose/fortune-cookie.png" alt="cookie" />
+                </div>
+                <div className="text-center col-12">
+                    <div className={phraseVisibility ? 'fadeIn-button':'fadeOut-button'}>
+                        <h4 className=""> {message} </h4>
+                        <div className={phraseVisibility ? 'fadeIn-button text-center p-3':'invisible'}>
+                            <button className="btn btn-outline-dark" onClick={handleReset}>
+                                {
+                                    language === "Eng" ? "Try again" : "Спробуй ще раз"
+                                } 
+                            </button>
                         </div>
                     </div>
                 </div>
--- src/Components/Language.js
+++ src/Components/Language.js
@@ -4,15 +4,13 @@ export default class Language extends Component {
     render() {
         const { languages, handleSubmit } = this.props;
         return (
-            <div className="container pt-3">
-                <div className="h-50">
-                    {
-                        languages.map((item, index) => {
-                            return (
-                                <button onClick={handleSubmit} className="btn btn-outline-dark m-1" value={index}>{item}</button>
-                            );
-                    })}
-                </div>
+            <div className="text-center p-3">
+                {
+                    languages.map((item, index) => {
+                        return (
+                            <button onClick={handleSubmit} className="btn btn-outline-dark m-1" value={index}>{item}</button>
+                        );
+                })}
             </div>
         )
     }
